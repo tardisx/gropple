9944cb9104ceeebe6cf6ab6d288c1065d03846c3
Justin Hawkins <justin@hawkins.id.au>
2023-03-09 21:32:24 +1030

    Appease linter

--- download/download.go
+++ download/download.go
@@ -153,7 +153,6 @@ func (m *Manager) AddDownload(dl *Download) {
 	m.Lock.Lock()
 	defer m.Lock.Unlock()
 	m.Downloads = append(m.Downloads, dl)
-	return
 }
 
 // func (dl *Download) AppendLog(text string) {
